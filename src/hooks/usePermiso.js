3af4e2d0808a5fa493bac2aa4f79d57eb8d4b4b1
JDPR19 <programandopanconqueso@hotmail.com>
2025-10-24 19:53:59 -0400

    resulto bug

--- src/hooks/usePermiso.js
+++ src/hooks/usePermiso.js
@@ -1,6 +1,9 @@
 export default function usePermiso() {
-  const user = JSON.parse(localStorage.getItem('user'));
-  const permisos = user?.permisos || {};
+  let permisos = {};
+  if (typeof window !== "undefined") {
+    const user = JSON.parse(localStorage.getItem('user'));
+    permisos = user?.permisos || {};
+  }
   return (pantalla, accion) => {
     if (!permisos[pantalla]) return false;
     return !!permisos[pantalla][accion];
