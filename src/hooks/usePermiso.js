a106744b0e2c635730d288f5ea4e7c3d577d35c7
JDPR19 <programandopanconqueso@hotmail.com>
2025-10-24 20:01:57 -0400

    bug nuevamente resueltoo por segunda vez

--- src/hooks/usePermiso.js
+++ src/hooks/usePermiso.js
@@ -1,11 +1,10 @@
-export default function usePermiso() {
-  let permisos = {};
-  if (typeof window !== "undefined") {
-    const user = JSON.parse(localStorage.getItem('user'));
-    permisos = user?.permisos || {};
-  }
+export const usePermiso = () => {
+  const permisos =
+    typeof window !== "undefined"
+      ? (JSON.parse(localStorage.getItem('user'))?.permisos || {})
+      : {};
   return (pantalla, accion) => {
     if (!permisos[pantalla]) return false;
     return !!permisos[pantalla][accion];
   };
-}
+};
